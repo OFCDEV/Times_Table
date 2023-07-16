a03ec1d83a71f33a0f77c3ac557593d596114dbd
Debasish Bhola <devop69@gmail.com>
2023-07-16 20:28:04 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/timestable/MainActivity.java
+++ app/src/main/java/com/example/timestable/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.timestable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.SeekBar;
 
 import com.example.timetable.R;
@@ -15,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        ListView timesTableListView = (ListView) findViewById(R.id.timesTableListView);
+
+        seekBar.setMax(20);
+        seekBar.setProgress(10);
+        //Creating method for seekbar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
